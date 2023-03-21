7b1c43799ae8138955951588688be45bb4f0224b
HoSHIZA <needahentai@gmail.com>
2023-03-22 00:21:57 +0300

    Added ServiceLocator

---
+++ Assets/Game/Scripts/Runtime/Core/Base/ServiceLocator/Impl/StaticServiceLocator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Game.Core.Base.ServiceLocator
+{
+    /// <summary>
+    /// Static implementation of a IServiceLocator that manages services.
+    /// </summary>
+    public sealed class StaticServiceLocator : IServiceLocator
+    {
+        /// <inheritdoc/>
+        public void RegisterSingle<TService, TImpl>() where TService : IService where TImpl : TService
+        {
+            Implementation<TService>.ServiceInstance = Activator.CreateInstance<TImpl>();
+        }
+        
+        /// <inheritdoc/>
+        public void RegisterSingle<TService>(TService instance) where TService : IService
+        {
+            Implementation<TService>.ServiceInstance = instance;
+        }
+        
+        /// <inheritdoc/>
+        public TService Single<TService>() where TService : IService
+        {
+            return Implementation<TService>.ServiceInstance;
+        }
+        
+        private static class Implementation<TService> where TService : IService
+        {
+            public static TService ServiceInstance;
+        }
+    }
+}
